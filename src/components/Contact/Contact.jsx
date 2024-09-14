8db427265b5618b04cbe80ab38726196f29c7839
leducanh112 <duca1122001@gmail.com>
2024-09-15 03:32:40 +0700

    test version

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:duca1122001@gmail.com">duca1122001@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="Linkedin icon"
+          />
+          <a href="https://www.linkedin.com/in/%C4%91%E1%BB%A9c-anh-3bb156197/">
+            linkedin.com/DucAnh
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/leducanh112">github.com/leducanh112</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Contact;
